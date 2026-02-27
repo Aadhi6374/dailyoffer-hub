4b4db666e88385d0b74aa62bcf4f4d879f53279e
Aadhi6374 <m.aadhi90038970@gmail.com>
2026-02-27 21:37:03 +0530

    Refactor product rendering and cart functionality

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const products = [
         price: 40.66, 
         rating: 4, 
         image: "https://img.forestessentialsindia.com/pub/media/catalog/product/cache/f8158826193ba5faa8b862a9bd1eb9e9/8/8/8842_soundarya_radiance_day_cream_50g_front_2048_x_2048.png", 
-        amazon: "https://amzn.to/4sbSjai"
+        amazon: "https://amzn.to/40zodlq"
     }
 ];
 
@@ -116,3 +116,4 @@ function updateCartUI() {
 
 // Load products on start
 renderProducts();
+
